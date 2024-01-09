d6543a7852070586bb3105ea4d9b90bf7d23e927
Greiskel <greiskel.sanchez@gmail.com>
2024-01-09 17:53:28 +0100

    footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -7,22 +7,22 @@ const Footer = () => {
     <footer>
       <div className="container">
         <div className="column">
-          <div className="logo">
-             <img src="../assets/Imagenes/logonegro.png" height="60px" alt="Factoria F5" /> 
+          <div>
+             <img className="logo" src="../assets/Imagenes/logonegro.png" alt="Factoria F5" /> 
           </div>
           <p>Somos y Factoria F5</p>
           <p>Calle Fernando Poo 25</p>
           <p>Madrid, 28045</p>
         </div>
         <div className="column">
-          <Nav.Link href="/">Home</Nav.Link>
-          <Nav.Link href="https://www.factoriaf5.org/somos" target="_blank">Factoria F5</Nav.Link>
-          <Nav.Link href="https://www.somosf5.org/about" target="_blank">Somos F5</Nav.Link>
-          <Nav.Link href="/Contact">Contacto</Nav.Link>
+          <Nav.Link href="/"><strong>Home</strong></Nav.Link>
+          <Nav.Link href="https://www.factoriaf5.org/somos" target="_blank"><strong>Factoria F5</strong></Nav.Link>
+          <Nav.Link href="https://www.somosf5.org/about" target="_blank"><strong>Somos F5</strong></Nav.Link>
+          <Nav.Link href="/Contact"><strong>Contacto</strong></Nav.Link>
         </div>
         <div className="column">
-          <div className="logo">
-            <img className="logo" src="../assets/logonaranja.png" href="/" height="60px" alt="Segundo Logo" />
+          <div>
+            <img className="logo" src="../assets/logonaranja.png" href="/" alt="Segundo Logo" />
           </div>
           <div className="redes-sociales">
             <a href="/"><img src="../assets/union.png" alt="Facebook" /></a>
